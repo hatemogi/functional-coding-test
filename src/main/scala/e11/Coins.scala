32b3dea71d576c26f2460137f6c0d36ea645be12
김대현 <hatemogi@gmail.com>
2020-01-11 18:32:22 +0900

    동전 문제 풀이

--- src/main/scala/e11/Coins.scala
+++ src/main/scala/e11/Coins.scala
@@ -2,7 +2,18 @@ package e11
 
 object Coins extends App {
   def minCoins(coins: List[Int], total: Int): List[Int] = {
-    Nil
+    def select(leftCoins: List[Int], selected: List[Int], amount: Int): List[List[Int]] = {
+      if (amount == total) List(selected)
+      else if (amount > total || leftCoins.isEmpty) Nil
+      else {
+        select(leftCoins, leftCoins.head :: selected, leftCoins.head + amount) ++
+        select(leftCoins.tail, selected, amount)
+      }
+    }
+    select(coins, Nil, 0)
+      .minByOption(_.size)
+      .getOrElse(Nil)
+      .sorted(Ordering[Int].reverse)
   }
 
   println(minCoins(List(1, 3, 4), 6))
--- src/test/scala/e11/CoinsSpec.scala
+++ src/test/scala/e11/CoinsSpec.scala
@@ -7,5 +7,7 @@ class CoinsSpec extends AnyFlatSpec {
 
   "minCoins" should "최소의 동전으로 원하는 액수를 만들어야 한다" in {
     assert(minCoins(List(1, 3, 4), 6) == List(3, 3))
+    assert(minCoins(List(1, 3, 7), 5) == List(3, 1, 1))
+    assert(minCoins(List(2), 1) == List())
   }
 }
