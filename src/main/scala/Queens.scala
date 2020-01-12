6bc9191cf19a99cc548ab02de35b08d4bd69d7fe
김대현 <hatemogi@gmail.com>
2020-01-12 12:31:00 +0900

    퀸공격판단 함수 밖으로 빼고, 정렬 조건 추가

--- src/main/scala/Queens.scala
+++ src/main/scala/Queens.scala
@@ -1,15 +1,14 @@
 object Queens extends App {
+  @scala.annotation.tailrec
+  def attacked(qs: List[Int]): Boolean = qs match {
+    case Nil => false
+    case h :: t =>
+      t.zipWithIndex.exists {
+        case (q, i) => List(q, q - (i + 1), q + i + 1) contains h
+      } || (attacked(qs.tail))
+  }
+
   def queens(n: Int): List[List[Int]] = {
-
-    @scala.annotation.tailrec
-    def attacked(qs: List[Int]): Boolean = qs match {
-      case Nil => false
-      case h :: t =>
-        t.zipWithIndex.exists {
-          case (q, i) => List(q, q - (i + 1), q + i + 1) contains h
-        } || (attacked(qs.tail))
-    }
-
     def assign(count: Int, assigned: List[Int]): List[List[Int]] =
       if (attacked(assigned)) Nil
       else if (count <= 0) List(assigned)
@@ -23,8 +22,8 @@ object Queens extends App {
   (1 to 8).foreach(pqueens)
 
   private def pqueens(n: Int): Unit = {
-    val qs = queens(n)
-    println(s"queens($n) => (${qs.size})\n" +
+    val qs = queens(n).sortBy { qs => qs.foldLeft(1) { (s, q) => s * n + q }}
+    println(s"queens($n) => ${qs.size}\n" +
       (if (qs.isEmpty) " []" else qs.mkString(" [", "\n  ", "]")))
   }
 
