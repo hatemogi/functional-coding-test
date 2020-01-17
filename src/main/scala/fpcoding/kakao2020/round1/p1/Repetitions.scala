317bba0876f807eaffc6afcbefb791e9cc035c11
Daehyun Kim <hatemogi@gmail.com>
2020-01-18 01:31:03 +0900

    1차 스칼라 풀이 추가

--- src/main/scala/fpcoding/kakao2020/round1/p1/Repetitions.scala
+++ src/main/scala/fpcoding/kakao2020/round1/p1/Repetitions.scala
@@ -2,18 +2,31 @@ package fpcoding.kakao2020.round1.p1
 
 // https://tech.kakao.com/2019/10/02/kakao-blind-recruitment-2020-round1/
 object Repetitions extends App {
-  def compress(n: Int, cs: List[List[Char]]): (Int, String) = {
-    cs match {
-      case Nil => (n, "")
-      case first :: second :: _ => (n, "")
+  def compress(cs: List[List[Char]]): String = {
+    if (cs.isEmpty) ""
+    else {
+      val reps = cs.takeWhile(cs.head == _)
+      (if (reps.size == 1) "" else reps.size.toString) +
+      cs.head.mkString("") +
+      compress(cs.drop(reps.size))
     }
   }
 
-  def compress(s: String): (Int, String) = {
+  def compress(s: String): String = {
     val cs = s.toList
     (1 to cs.size / 2)
-      .map { n => compress(n, cs.grouped(n).toList) }
-      .minBy(_._2.length)
+      .map { n => compress(cs.grouped(n).toList) }
+      .minBy(_.length)
   }
 
+  private def pcompress(s: String) = {
+    val compressed = compress(s)
+    println(s"compress(${s}) = ${compressed.length}, $compressed")
+  }
+
+  pcompress("aabbaccc")
+  pcompress("ababcdcdababcdcd")
+  pcompress("abcabcdede")
+  pcompress("abcabcabcabcdededededede")
+  pcompress("xababcdcdababcdcd")
 }
