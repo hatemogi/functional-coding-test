c55bda084d6b026202930912b8369c6dbf4c0eac
Daehyun Kim <hatemogi@gmail.com>
2020-01-19 00:29:43 +0900

    2020 1라운드 1번문제 기본 예제 테스트케이스 추가

--- src/main/scala/fpcoding/kakao2020/round1/p1/Repetitions.scala
+++ src/main/scala/fpcoding/kakao2020/round1/p1/Repetitions.scala
@@ -2,24 +2,21 @@ package fpcoding.kakao2020.round1.p1
 
 // https://tech.kakao.com/2019/10/02/kakao-blind-recruitment-2020-round1/
 object Repetitions extends App {
-  def compress(cs: List[List[Char]]): String = {
+  def compress(cs: List[String]): String = {
     if (cs.isEmpty) ""
     else {
-      val reps = cs.takeWhile(cs.head == _)
-      (if (reps.size == 1) "" else reps.size.toString) +
-      cs.head.mkString("") +
-      compress(cs.drop(reps.size))
+      val reps = cs.takeWhile(cs.head == _).size
+      (if (reps == 1) "" else reps.toString) + cs.head +
+      compress(cs.drop(reps))
     }
   }
 
-  def compress(s: String): String = {
-    val cs = s.toList
-    (1 to cs.size / 2)
-      .map { n => compress(cs.grouped(n).toList) }
+  def compress(s: String): String =
+    (1 to s.length / 2)
+      .map { n => compress(s.grouped(n).toList) }
       .minBy(_.length)
-  }
 
-  private def pcompress(s: String) = {
+  private def pcompress(s: String): Unit = {
     val compressed = compress(s)
     println(s"compress(${s}) = ${compressed.length}, $compressed")
   }
--- src/test/scala/fpcoding/kakao2020/round1/p1/RepetitionSpec.scala
+++ src/test/scala/fpcoding/kakao2020/round1/p1/RepetitionSpec.scala
@@ -5,8 +5,10 @@ import org.scalatest.flatspec.AnyFlatSpec
 class RepetitionSpec extends AnyFlatSpec {
   import Repetitions.compress
   "기본예제#1" should "주어진 답변과 같아야" in {
-    assert(compress("aabbaccc") == (1, "2a2ba3c"))
+    assert(compress("aabbaccc").length == 7)
+    assert(compress("ababcdcdababcdcd").length == 9)
+    assert(compress("abcabcdede").length == 8)
+    assert(compress("abcabcabcabcdededededede").length == 14)
+    assert(compress("xababcdcdababcdcd").length == 17)
   }
-
-
 }
