3cedb0dc06d5c34effb7649a839254cbcd29415a
김대현 <hatemogi@gmail.com>
2020-02-06 15:43:07 +0900

    벨만 포드 풀이 추가

---
+++ src/main/scala/fpcoding/BellmanFord.scala
@@ -0,0 +1,42 @@
+package fpcoding
+
+/**
+ * 벨만-포드 알고리즘은 노드 x에서 다른 모든 노드로 가는 최단 경로를 찾는다.
+ */
+object BellmanFord extends App {
+  type Node = Int                       // 노드ID
+  type Edge = (Node, Node, Int)         // Node간 거리
+  type Graph = List[Edge]               // 그래프의 Edge 모두 관리
+  type Distances = Map[Node, Int]
+
+  val example: Graph = List(
+    (1, 2, 2),
+    (1, 3, 3),
+    (1, 4, 7),
+    (2, 4, 3),
+    (2, 5, 5),
+    (3, 4, 1),
+    (4, 5, 2)
+  )
+
+  def nodes(g: Graph): Set[Node] =
+    g.foldLeft(Set.empty[Node]) { (set, edge) =>
+      set + edge._1 + edge._2
+    }
+
+  def shortest(starting: Node, g: Graph): Distances = {
+    val initial: Distances = Map[Node, Int](starting -> 0)
+    nodes(g).foldLeft(initial) { case (distances, _) =>
+      g.foldLeft(distances) { case (distances, (from, to, weight)) =>
+        val newLength = distances.get(from).map(_ + weight)
+        if (newLength.exists(l => l <= distances.getOrElse(to, l)))
+          distances.updated(to, newLength.get)
+        else
+          distances
+      }
+    }
+  }
+
+  println(shortest(1, example))
+
+}
